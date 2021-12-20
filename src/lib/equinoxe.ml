876908f8a5f2065481afa5a9a83b8075747afdc6
Etienne MARAIS <etienne@maiste.fr>
2021-12-20 12:08:36 +0100

    lib: support delete api keys for users

--- src/lib/equinoxe.ml
+++ src/lib/equinoxe.ml
@@ -42,12 +42,16 @@ module Make (C : CallAPI.S) = struct
       let path = "user/api-keys" in
       C.get ~path t () |> C.run
 
-    let add_user_api_keys t ?(read_only = true) description =
+    let add_user_api_key t ?(read_only = true) description =
       let read_only = ("read_only", ~+(string_of_bool read_only)) in
       let description = ("description", ~+description) in
       let json = Json.create () -+> read_only -+> description in
       let path = "user/api-keys" in
       C.post t ~path json |> C.run
+
+    let del_user_api_key t key_id =
+      let path = Filename.concat "user/api-keys/" key_id in
+      C.delete t ~path () |> C.run
   end
 
   module Orga = struct end
--- src/lib/equinoxe_intf.ml
+++ src/lib/equinoxe_intf.ml
@@ -45,9 +45,13 @@ module type API = sig
     val get_user_api_keys : t -> Json.t
     (** [get_user_api_keys t] returns the keys available for the current user. *)
 
-    val add_user_api_keys : t -> ?read_only:bool -> string -> Json.t
-    (** [add_user_api_keys t ~read_only description] creates a new API key on
+    val add_user_api_key : t -> ?read_only:bool -> string -> Json.t
+    (** [add_user_api_key t ~read_only description] creates a new API key on
         Equinix. Default value to read_only is true. *)
+
+    val del_user_api_key : t -> string -> Json.t
+    (** [del_user_api_key t key_id ] deletes the key referenced by [key_id] from
+        the user keys. *)
   end
 
   module Orga : sig end
