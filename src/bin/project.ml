89f70de7e9992c70d43944b0bac7b8eb110c1c4c
Etienne MARAIS <etienne@maiste.fr>
2021-12-23 11:27:44 +0100

    bin: add getters devices

--- src/bin/project.ml
+++ src/bin/project.ml
@@ -48,6 +48,17 @@ let projects_id meth id =
       else not_all_requiered_r [ "id" ]
   | meth -> not_supported_r meth "/projects/{id}"
 
+let projects_id_devices meth id =
+  let endpoint = Conf.endpoint in
+  let e = Equinoxe.create ~endpoint () in
+  match meth with
+  | GET ->
+      if has_requiered [ id ] then
+        let id = Option.get id in
+        Equinoxe.Projects.get_projects_id_devices e ~id () |> Json.pp_r
+      else not_all_requiered_r [ "id" ]
+  | meth -> not_supported_r meth "/projects/{id}/devices"
+
 (* Terms *)
 
 let projects_t =
@@ -62,8 +73,10 @@ let projects_t =
 let projects_id_t =
   let doc = "Show the project of the user referenced by the id" in
   let exits = default_exits in
-  let man = man_meth ~get:"Retrieve information about a specific project." () in
-
+  let man =
+    man_meth
+      ~get:"Retrieve information about a specific project. Argument(s): id" ()
+  in
   let id_t =
     let doc = "The project id" in
     Arg.(value & opt (some string) None & info [ "id" ] ~doc)
@@ -72,4 +85,24 @@ let projects_id_t =
     ( term_result (const projects_id $ meth_t $ id_t),
       info "/projects/id" ~doc ~exits ~man )
 
-let t = [ projects_t; projects_id_t ]
+let projects_id_devices_t =
+  let doc =
+    "Show all devices on the project of the user referenced by the id"
+  in
+  let exits = default_exits in
+  let man =
+    man_meth
+      ~get:
+        "Retrieve information about about devices associated with a specific \
+         project. Argument(s): id."
+      ()
+  in
+  let id_t =
+    let doc = "The project id" in
+    Arg.(value & opt (some string) None & info [ "id" ] ~doc)
+  in
+  Term.
+    ( term_result (const projects_id $ meth_t $ id_t),
+      info "/projects/id/devices" ~doc ~exits ~man )
+
+let t = [ projects_t; projects_id_t; projects_id_devices_t ]
