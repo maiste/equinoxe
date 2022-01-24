f7fa4728fc91a7bc7683630e1719ab48b9be3076
ArthurW <arthur@tarides.com>
2022-01-24 17:21:23 +0100

    mock: FriendlyApi.Orga.get_from

--- tests/mock/equinoxe_mock.ml
+++ tests/mock/equinoxe_mock.ml
@@ -339,5 +339,91 @@ let test_orga_get_all =
       assert (expected.E.Orga.id = orga.E.Orga.id))
     expected orgas
 
-let test_orga = [ ("orga", [ test_orga_get_all ]) ]
+let test_orga_get_from =
+  Alcotest.test_case "Orga.get_from" `Quick @@ fun () ->
+  let raw_json =
+    {|{
+        "id": "mock-id-1",
+        "name": "Mock Orga 1",
+        "description": null,
+        "website": "",
+        "twitter": "",
+        "created_at": "2021-12-20T10:47:12Z",
+        "updated_at": "2022-01-19T08:52:08Z",
+        "tax_id": null,
+        "main_phone": null,
+        "billing_phone": null,
+        "credit_amount": 0,
+        "personal": false,
+        "customdata": {},
+        "attn": null,
+        "purchase_order": null,
+        "billing_name": null,
+        "enforce_2fa": false,
+        "enforce_2fa_at": null,
+        "short_id": "mock short id 1",
+        "account_id": "mock account id 1",
+        "enabled_features": [
+          "maintenance_mail",
+          "deploy_without_public_ip",
+          "advanced_ips",
+          "..."
+        ],
+        "maintenance_email": "admin@mock1.fr",
+        "abuse_email": "admin@mock1.fr",
+        "legal_company_name": "",
+        "max_projects": 1,
+        "default_collaborator_role": null,
+        "address": {
+          "href": "#mock-address-1"
+        },
+        "billing_address": {
+          "href": "#mock-billing-1"
+        },
+        "account_manager": null,
+        "logo": null,
+        "logo_thumb": null,
+        "projects": [
+          {
+            "href": "/metal/v1/projects/mock-projects-1"
+          }
+        ],
+        "plan": "Starter",
+        "monthly_spend": 9,
+        "current_user_abilities": {
+          "admin": true,
+          "billing": true,
+          "collaborator": true,
+          "owner": false
+        },
+        "href": "/metal/v1/organizations/mock-href-1"
+      }|}
+  in
+  let module E =
+  (val mock_friendly [ (Get "organizations/mock-id-1", raw_json) ])
+  in
+  let t = E.create ~address ~token () in
+  let requested_id = E.Orga.id_of_string "mock-id-1" in
+  let orga = E.Orga.get_from t requested_id in
+  let expected =
+    E.Orga.
+      {
+        id = requested_id;
+        name = "Mock Orga 1";
+        account_id = "mock account id 1";
+        website = "";
+        maintenance_email = "admin@mock1.fr";
+        max_projects = 1;
+      }
+  in
+  let open E.Orga in
+  Alcotest.(check string) "name" expected.name orga.name;
+  Alcotest.(check string) "account id" expected.account_id orga.account_id;
+  Alcotest.(check string) "website" expected.website orga.website;
+  Alcotest.(check string)
+    "maintenance email" expected.maintenance_email orga.maintenance_email;
+  Alcotest.(check int) "max projects" expected.max_projects orga.max_projects;
+  assert (expected.E.Orga.id = orga.E.Orga.id)
+
+let test_orga = [ ("orga", [ test_orga_get_all; test_orga_get_from ]) ]
 let () = Alcotest.run "mock" (test_errors @ test_auth @ test_orga)
